338fec0e1f8cf5882b07eec3d01ca8befff884bf
juzten <juzten@gmail.com>
2022-06-14 21:00:44 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+// Convert temperatures between Fahrenheit and Celsius.
+// Generate the nth Fibonacci number.
+// Print the lyrics to the Christmas carol “The Twelve Days of Christmas,” taking advantage of the repetition in the song.
+
+
+fn main() {
+    println!("Hello, world!");
+
+    another_function(5);
+    assert_eq!(convert_f_to_c(70), 21);
+    assert_eq!(convert_c_to_f(70), 158);
+
+}
+
+fn another_function(x: i32) {
+    println!("another function: {}", x);
+}
+
+
+fn convert_f_to_c(temperature: i32) -> i32 {
+    ((temperature - 32) as f64 * 0.5556) as i32
+}
+
+fn convert_c_to_f(temperature: i32) -> i32 {
+    (temperature as f64 * 1.8) as i32 + 32
+}
