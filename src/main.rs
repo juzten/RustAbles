3ca6028a041f9705b3f23f2bccd357b19f772143
juzten <juzten@gmail.com>
2022-06-15 00:36:04 -0400

    structs and funcs

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,23 @@ fn main() {
     another_function(5);
     assert_eq!(convert_f_to_c(70), 21);
     assert_eq!(convert_c_to_f(70), 158);
+
+    let mut s = String::from("hello");
+    s.push_str(", world");
+    println!("{}", s);
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
+    if rect1.width() {
+        println!("And the width is {}.", rect1.width)
+    }
+
+    let x = define_x();
+    println!("{}, world", x);
 }
 
 fn another_function(x: i32) {
@@ -18,6 +35,27 @@ fn convert_f_to_c(temperature: i32) -> i32 {
 
 fn convert_c_to_f(temperature: i32) -> i32 {
     (temperature as f64 * 1.8) as i32 + 32
+}
+
+fn define_x() -> String {
+    let x = "hello".to_string();
+    x
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
 }
 
 #[cfg(test)]
